e9084bc8cd9a7ba731fd527d5c7f8749b68c929b
Mai Quốc Việt <vietmq.21it@vku.udn.vn>
2025-02-19 16:03:20 +0700

    update admin product

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Category, Product, ProductImage
+from django.utils.html import format_html
 
 # Admin cho Category
 class CategoryAdmin(admin.ModelAdmin):
@@ -13,18 +14,22 @@ admin.site.register(Category, CategoryAdmin)
 
 # Admin cho Product
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock', 'size', 'color', 'category', 'created_at', 'updated_at')
-    search_fields = ('name', 'description', 'category__name', 'color')
+    list_display = ('name', 'price', 'stock', 'size', 'color', 'category','avatar_preview')
+    search_fields = ('name__icontains', 'category__name__icontains', 'color__icontains')
     list_filter = ('size', 'category', 'created_at')
-    ordering = ('-created_at',)
-    list_editable = ('price', 'stock', 'size', 'color')  # Cho phép chỉnh sửa trực tiếp trong danh sách
+    list_editable = ( 'size',)  # Cho phép chỉnh sửa trực tiếp trong danh sách
     
-    # Cập nhật lại fieldsets để loại bỏ các trường created_at và updated_at
     fieldsets = (
         (None, {
             'fields': ('name', 'description', 'price', 'stock', 'size', 'color', 'category', 'image')
         }),
     )
+
+    def avatar_preview(self, obj):
+        """Hiển thị ảnh avatar trong danh sách admin"""
+        if obj.image:
+            return format_html('<img src="{}" width="100" height="100" style="border-radius:5px;" />', obj.image.url)
+        return "(Không có ảnh)"
 
 admin.site.register(Product, ProductAdmin)
 
