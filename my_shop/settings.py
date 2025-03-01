726496483ba1f81e66ad499c29f31d654b39aaa4
Mai Quốc Việt <vietmq.21it@vku.udn.vn>
2025-03-01 16:36:12 +0700

    create library

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 
-python manage.py collectstatic
 """
 
 from pathlib import Path
