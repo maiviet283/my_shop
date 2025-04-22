d2d4932ec0bff4f69c180f72a2e102d25bb2a868
Mai Quốc Việt <vietmq.21it@vku.udn.vn>
2025-04-22 20:02:19 +0700

    update time JWT access

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -16,7 +16,7 @@ import os
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),  # Thời gian sống của Access Token
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),  # Thời gian sống của Access Token
     'REFRESH_TOKEN_LIFETIME': timedelta(minutes=20),     # Thời gian sống của Refresh Token
     'ROTATE_REFRESH_TOKENS': True,                 # Có tự động làm mới Refresh Token khi làm mới Access Token hay không
     'BLACKLIST_AFTER_ROTATION': True,               # Có vô hiệu hóa Refresh Token sau khi nó được làm mới hay không
