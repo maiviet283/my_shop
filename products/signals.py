ec9f3aec02d33a1e6ddee6b53d4ca4c85b93da61
Mai Quốc Việt <vietmq.21it@vku.udn.vn>
2025-09-09 14:07:58 +0700

    update variable in redis

--- products/signals.py
+++ products/signals.py
@@ -1,48 +1,67 @@
-from django.db.models.signals import pre_save, post_delete
+from django.db.models.signals import pre_save, post_delete, post_save, post_delete
 from django.dispatch import receiver
-from .models import Product, ProductImage
+from django.core.cache import cache
+from .models import Product, ProductImage, Category
 import os
 
-# Product
-# Xóa avatar cũ khi cập nhật ảnh mới
-@receiver(pre_save, sender=Product)
-def delete_old_avatar_on_update(sender, instance, **kwargs):
-    if instance.pk:
-        try:
-            old_avatar = Product.objects.get(pk=instance.pk).image
-        except Product.DoesNotExist:
-            return
-        new_avatar = instance.image
-        if old_avatar and old_avatar != new_avatar and "default.png" not in old_avatar.name:
-            if os.path.isfile(old_avatar.path):  # Dùng `.path` thay vì `.name`
-                os.remove(old_avatar.path)
 
-# Xóa avatar khi xóa sản phẩm
+@receiver(pre_save, sender=Product)
+def delete_old_product_image(sender, instance, **kwargs):
+    """Xóa ảnh cũ khi cập nhật Product.image"""
+    if not instance.pk:
+        return
+
+    try:
+        old_image = Product.objects.get(pk=instance.pk).image
+    except Product.DoesNotExist:
+        return
+
+    new_image = instance.image
+    if old_image and old_image != new_image:
+        if os.path.isfile(old_image.path):
+            os.remove(old_image.path)
+
+
 @receiver(post_delete, sender=Product)
-def delete_avatar_on_delete(sender, instance, **kwargs):
-    if instance.image and "default.png" not in instance.image.name:
-        if os.path.isfile(instance.image.path):  # Dùng `.path`
-            os.remove(instance.image.path)
- 
+def delete_product_image_on_delete(sender, instance, **kwargs):
+    """Xóa ảnh khi Product bị xóa"""
+    if instance.image and os.path.isfile(instance.image.path):
+        os.remove(instance.image.path)
+
 
 # ProductImage
 @receiver(pre_save, sender=ProductImage)
-def delete_old_image_on_update(sender, instance, **kwargs):
-    """Xóa ảnh cũ nếu ảnh mới được tải lên"""
-    if instance.pk:  # Nếu sản phẩm đã tồn tại (để tránh trường hợp create mới)
-        try:
-            old_image = ProductImage.objects.get(pk=instance.pk).image
-        except ProductImage.DoesNotExist:
-            return
-        new_image = instance.image
-        if old_image and old_image != new_image:  # Chỉ xóa nếu ảnh thực sự thay đổi
-            if os.path.isfile(old_image.path):
-                os.remove(old_image.path)
+def delete_old_productimage_file(sender, instance, **kwargs):
+    """Xóa ảnh cũ khi cập nhật ProductImage.image"""
+    if not instance.pk:
+        return
 
-# Xóa ảnh khi xóa ProductImage
+    try:
+        old_image = ProductImage.objects.get(pk=instance.pk).image
+    except ProductImage.DoesNotExist:
+        return
+
+    new_image = instance.image
+    if old_image and old_image != new_image:
+        if os.path.isfile(old_image.path):
+            os.remove(old_image.path)
+
+
 @receiver(post_delete, sender=ProductImage)
-def delete_image_on_delete(sender, instance, **kwargs):
-    """Xóa ảnh khỏi hệ thống khi ProductImage bị xóa"""
-    if instance.image:
-        if os.path.isfile(instance.image.path):
-            os.remove(instance.image.path)
+def delete_productimage_file_on_delete(sender, instance, **kwargs):
+    """Xóa ảnh khi ProductImage bị xóa"""
+    if instance.image and os.path.isfile(instance.image.path):
+        os.remove(instance.image.path)
+
+
+@receiver([post_save, post_delete], sender=Product)
+def clear_product_cache(sender, instance, **kwargs):
+    cache_key = f"product_{instance.pk}"
+    cache.delete(cache_key)
+
+    for page in range(1, 10):  # giả sử tối đa 10 trang
+        cache.delete(f"product_list_page_{page}")
+
+@receiver([post_save, post_delete], sender=Category)
+def clear_category_cache(sender, instance, **kwargs):
+    cache.delete("category_list")
